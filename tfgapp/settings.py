61b9556170135bbbb3daeb8ede91ad16afc09639
miwe <miwesfx@gmail.com>
2024-07-06 12:43:37 +0200

    Mejoras estéticas y carga de fotos

--- tfgapp/settings.py
+++ tfgapp/settings.py
@@ -132,6 +132,10 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, "static/")
 
+# Para la carga de fotos
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
